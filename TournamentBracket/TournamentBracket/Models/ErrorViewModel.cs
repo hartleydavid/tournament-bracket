bd5afe8dbc4e2b74c0541a29939bb597eb4affd2
hartleydavid <56169730+hartleydavid@users.noreply.github.com>
2024-04-11 17:54:26 -0700

    Added empty ASP.NET MVC project
    May or may not change the framework once development begins.

---
+++ TournamentBracket/TournamentBracket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TournamentBracket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
